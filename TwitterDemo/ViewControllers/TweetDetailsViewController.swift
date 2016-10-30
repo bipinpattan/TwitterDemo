ef8da013b30322dc57d5df3352dc834f026e084b
Bipin Pattan <bpattan@couponsinc.com>
2016-10-30 13:48:22 -0700

    Add a tweet details VC.

---
+++ TwitterDemo/ViewControllers/TweetDetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TweetDetailsViewController.swift
+//  TwitterDemo
+//
+//  Created by Bipin Pattan on 10/30/16.
+//  Copyright © 2016 Bipin Pattan. All rights reserved.
+//
+
+import UIKit
+
+class TweetDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onDoneButton(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+}
